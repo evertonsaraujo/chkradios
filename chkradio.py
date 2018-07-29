82e4ea1c3ee9380094ce9a138fac935e4a353241
evertonsaraujo <40547014+evertonsaraujo@users.noreply.github.com>
2018-07-29 18:35:55 -0300

    Update chkradio.py

--- chkradio.py
+++ chkradio.py
@@ -1,23 +1,30 @@
-# Parametros iniciais de comunicacao TCP #
 import socket, os
 
 ports = [22, 80, 443, 4343, 2222, 8181]
 
 arq = open('ips-radios.txt','r')
+arq1 = open('ips-vuneraveis.txt','w')
 linha = arq.readline()
 ips = linha.split()
+iptmp = ''
 
 for ip in ips:
         response = os.system('ping -c 1 ' + ip + '>> /dev/null')
         if response == 0:
             for port in ports:
                 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                client.settimeout(0.1)
+                client.settimeout(0.3)
                 code = client.connect_ex((ip, port))
                 if code == 0:
-                    print (str(ip) + ' - ' + str(port) + (" -> Porta Aberta -- VUNERAVEL"))
+                     print(str(ip) +' '+ str(port) + ' -> Porta Aberta -- VUNERAVEL')
+                     if iptmp != ip:
+                       os.system('echo ' + ip + '>> ips-vuneraveis.txt')
+                     else:
+                           pass
+                     iptmp = ip
                 else:
-                    pass
+                     pass
         else:
             print(str(ip) + ' is DOWN')
 arq.close()
+arq1.close()
