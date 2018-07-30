afcde3157c1ff153468651cf0b8c831f2492698c
evertonsaraujo <40547014+evertonsaraujo@users.noreply.github.com>
2018-07-29 21:12:01 -0300

    Correçao espaços

--- chkradio.py
+++ chkradio.py
@@ -1,30 +1,31 @@
-import socket, os
+import socket
+import os
 
 ports = [22, 80, 443, 4343, 2222, 8181]
 
-arq = open('ips-radios.txt','r')
-arq1 = open('ips-vuneraveis.txt','w')
+arq = open('ips-radios.txt', 'r')
+arq1 = open('ips-vuneraveis.txt', 'w')
 linha = arq.readline()
 ips = linha.split()
 iptmp = ''
 
 for ip in ips:
-        response = os.system('ping -c 1 ' + ip + '>> /dev/null')
-        if response == 0:
-            for port in ports:
-                client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                client.settimeout(0.3)
-                code = client.connect_ex((ip, port))
-                if code == 0:
-                     print(str(ip) +' '+ str(port) + ' -> Porta Aberta -- VUNERAVEL')
-                     if iptmp != ip:
-                       os.system('echo ' + ip + '>> ips-vuneraveis.txt')
-                     else:
-                           pass
-                     iptmp = ip
+    response = os.system('ping -c 2 ' + ip + '>> /dev/null')
+    if response == 0:
+        for port in ports:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.settimeout(0.3)
+            code = client.connect_ex((ip, port))
+            if code == 0:
+                print(str(ip) + ' ' + str(port) + ' -> Porta Aberta -- VUNERAVEL')
+                if iptmp != ip:
+                    os.system('echo ' + ip + '>> ips-vuneraveis.txt')
                 else:
-                     pass
-        else:
-            print(str(ip) + ' is DOWN')
+                    pass
+                iptmp = ip
+            else:
+                pass
+    else:
+        print(str(ip) + ' is DOWN')
 arq.close()
 arq1.close()
